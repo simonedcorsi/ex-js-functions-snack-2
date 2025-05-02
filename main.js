87c2bbce66d90004886480923ebc4e418099d519
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-05-02 20:39:36 +0200

    fix

--- main.js
+++ main.js
@@ -26,74 +26,52 @@ const quadrato = n => n * n;
 /* SNACK 3 */
 // Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
 
-function operazione(a, b, segno) {
-    return segno(a, b);
+const operazione = (a, b, segno) => {
+    segno(a, b);
 }
-
-const somma = (a, b) => a + b;
-const sottrazione = (a, b) => a - b;
-const moltiplicazione = (a, b) => a * b;
-
-console.log(operazione(5, 3, somma));          
+        
+operazione(2, 3, somma);
 
 
 /* SNACK 4 */
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
 
-function creaTimer(ms) {
-    return function () {
+function creaTimer(tempo) {
+    return () => {
         setTimeout(() => {
             console.log('Tempo scaduto!')
-        }, ms);
+        }, tempo);
     }
 }
 
-let timer = creaTimer(3000);
+const timer = creaTimer(3000);
 timer();
 
 
 /* SNACK 5 */
 // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
 
-function cronometro(secondi) {
-    const id = setInterval(() => {
-        console.log(secondi);
+function cronometro(messaggio) {
+    setInterval(() => {
+        console.log(messaggio);
     }, 1000);
-
-    return id;
 }
-
-const intervallo = cronometro('secondi');
-
-setTimeout(() => {
-  clearInterval(intervallo);
-  console.log("Intervallo fermato.");
-}, 5000);
-
+cronometro(ciao)
 
 /* SNACK 6 */
 // Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
 
 function creaContatoreAutomatico(intervallo) {
-    return function() {
-      let contatore = 0;
-      const id = setInterval(() => {
-        contatore++;
-        console.log(contatore);
-      }, intervallo);
-      
-      return id; 
-    };
+  let count = 0;
+  return () => {
+    setInterval(() => {
+      count++;
+      console.log(Il contatore è arrivato a ${ count });
+    }, intervallo)
+  }
 }
-
-const avviaContatore = creaContatoreAutomatico(1000); 
-const contatoreId = avviaContatore();
-
-setTimeout(() => {
-  clearInterval(contatoreId);
-  console.log("Contatore fermato.");
-}, 5000);
-
+const contaOgniSecondo = creaContatoreAutomatico(1000);
+contaOgniSecondo();
 
 /* SNACK 7 */
 // Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
