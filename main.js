7e377e0c3186ba7d01d6bd089f5252d91527e9f1
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-05-02 19:24:06 +0200

    snack 4 e 5

--- main.js
+++ main.js
@@ -18,14 +18,12 @@ const somma = (a, b) => a + b;
 
 
 /* SNACK 2 */
-
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
 
 const quadrato = n => n * n;
 
 
 /* SNACK 3 */
-
 // Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
 
 function operazione(a, b, segno) {
@@ -37,3 +35,42 @@ const sottrazione = (a, b) => a - b;
 const moltiplicazione = (a, b) => a * b;
 
 console.log(operazione(5, 3, somma));          
+
+
+/* SNACK 4 */
+// Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(ms) {
+    return function () {
+        setTimeout(() => {
+            console.log('Tempo scaduto!')
+        }, ms);
+    }
+}
+
+let timer = creaTimer(3000);
+timer();
+
+
+/* SNACK 5 */
+// Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+function cronometro(secondi) {
+    const id = setInterval(() => {
+        console.log(secondi);
+    }, 1000);
+
+    return id;
+}
+
+const intervallo = cronometro('secondi');
+
+setTimeout(() => {
+  clearInterval(intervallo);
+  console.log("Intervallo fermato.");
+}, 5000);
+
+
+/* SNACK 6 */
+// Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+
